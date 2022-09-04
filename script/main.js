d16f4b1666d3e2e9ed44c285cafad33fc72defff
Felipe Tavares <xfelipetavares@gmail.com>
2022-09-04 20:32:01 -0300

    alteracoes feitas

---
+++ script/main.js
@@ -0,0 +1,11 @@
+window.addEventListener(`scroll`, scrollando)
+
+
+function scrollando(){
+    let navbar = document.getElementById(`navbar`)
+    if(scrollY > 35){
+        navbar.classList.add(`scrolling`)
+    } else {
+        navbar.classList.remove(`scrolling`)
+    }
+}
